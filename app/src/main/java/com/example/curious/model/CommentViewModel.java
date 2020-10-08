009add53deee8bbf4035f6da94b70b5beef0e6f6
KaushikYenni <yk12@iitbbs.ac.in>
2020-10-08 19:06:27 +0530

    Added a ViewModel for the comment

---
+++ app/src/main/java/com/example/curious/model/CommentViewModel.java
@@ -0,0 +1,10 @@
+package com.example.curious.model;
+
+import androidx.lifecycle.ViewModel;
+
+public class CommentViewModel extends ViewModel {
+    public CommentData commentData;
+    public String intentString;
+    public String id;
+    public Throwable throwable;
+}
