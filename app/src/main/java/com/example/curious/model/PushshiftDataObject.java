5d62c7df9fb2bfc47f33ededf2d3eb59f742cd41
KaushikYenni <yk12@iitbbs.ac.in>
2020-10-08 19:03:56 +0530

    Created a class for pushshift data object

---
+++ app/src/main/java/com/example/curious/model/PushshiftDataObject.java
@@ -0,0 +1,16 @@
+package com.example.curious.model;
+
+import java.util.List;
+
+public class PushshiftDataObject {
+    private List<CommentData> data;
+
+    public List<CommentData> getData() {
+        return data;
+    }
+
+    public void setData(List<CommentData> data) {
+        this.data = data;
+    }
+
+}
